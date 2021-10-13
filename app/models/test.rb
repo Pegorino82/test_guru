789e32dfdadefa651304ad8174072a53ba213e39
Pegorino82 <eshkryabin@yandex.ru>
2021-10-13 22:37:05 +0300

    Update class method in Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,8 @@
 
 class Test < ApplicationRecord
   def self.by_category(title)
-    category = Category.where(title: title).first
-    where(category_id: category.id).order(title: :DESC).all
+    joins('JOIN categories ON tests.category_id = categories.id')
+      .where(categories: { title: title })
+      .order(title: :DESC).all
   end
 end
