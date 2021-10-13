e2465a0a6d36f2f86081872158c123dff172d7b6
Pegorino82 <eshkryabin@yandex.ru>
2021-10-13 09:45:15 +0300

    Add class method in Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class Test < ApplicationRecord
+  def self.by_category(title)
+    category = Category.where(title: title).first
+    where(category_id: category.id).order(title: :DESC).all
+  end
 end
