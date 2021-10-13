19213f47afa8ed9066cfbdf6a64e07adf0e0f2ee
Pegorino82 <eshkryabin@yandex.ru>
2021-10-13 09:45:03 +0300

    Add instance method in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  # ищем среди авторов, т.к. пока у теста нет проходивших его пользователей
+  def tests_by_level(level)
+    Test.where(author_id: id, level: level).all
+  end
 end
