225ddc79d6d7fb588ab3a7be266602081ab105d7
Pegorino82 <eshkryabin@yandex.ru>
2021-10-16 18:33:53 +0300

    Add associations + update methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User' # author_id
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
+
   def self.by_category(title)
-    joins('JOIN categories ON tests.category_id = categories.id')
-      .where(categories: { title: title })
-      .order(title: :DESC)
+    Category.find_by(title: title).tests.order(title: :DESC)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :author, class_name: "Test", foreign_key: "author_id"
+  has_many :tests_users
+  has_many :tests, through: :tests_users
+
   def tests_by_level(level)
-    Result.joins('JOIN tests ON results.test_id = tests.id')
-          .select('tests.title as title')
-          .where(user_id: id, tests: { level: level })
-          .order(title: :DESC)
-          .pluck(:title)
+    self.tests.where(level: level).order(title: :DESC).pluck(:title)
   end
 end
