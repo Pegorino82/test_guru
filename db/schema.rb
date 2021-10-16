430692b66e6129c52c8e2e260022c04b52fce086
Pegorino82 <eshkryabin@yandex.ru>
2021-10-16 18:31:53 +0300

    Create join table tests_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_163937) do
+ActiveRecord::Schema.define(version: 2021_10_16_144514) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -35,16 +35,6 @@ ActiveRecord::Schema.define(version: 2021_10_13_163937) do
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
-  create_table "results", force: :cascade do |t|
-    t.integer "score"
-    t.integer "user_id", null: false
-    t.integer "test_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["test_id"], name: "index_results_on_test_id"
-    t.index ["user_id"], name: "index_results_on_user_id"
-  end
-
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0
@@ -56,6 +46,13 @@ ActiveRecord::Schema.define(version: 2021_10_13_163937) do
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
+  create_table "tests_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.integer "score", default: 0
+    t.index ["user_id", "test_id"], name: "index_tests_users_on_user_id_and_test_id", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.integer "role"
@@ -65,8 +62,6 @@ ActiveRecord::Schema.define(version: 2021_10_13_163937) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
-  add_foreign_key "results", "tests"
-  add_foreign_key "results", "users"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id"
 end
