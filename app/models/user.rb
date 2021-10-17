35964c8e06f9cbe841fd0e2593fefdd5bdaea8fb
Pegorino82 <eshkryabin@yandex.ru>
2021-10-17 20:49:07 +0300

    Delete behaviour

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :authorship, class_name: "Test", foreign_key: "author_id"
-  has_many :results
-  has_many :tests, through: :results
+  has_many :authorship, class_name: "Test", foreign_key: "author_id", dependent: :nullify
+  has_many :results, dependent: :destroy
+  has_many :tests, through: :results, dependent: :destroy
 
   def tests_by_level(level)
     self.tests.where(level: level).order(title: :DESC).pluck(:title)
