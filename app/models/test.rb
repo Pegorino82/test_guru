550e4f89e410d78a5dcd98164abea66abeee1874
Pegorino82 <eshkryabin@yandex.ru>
2021-10-17 19:10:33 +0300

    Code style

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,8 @@
 
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :author, class_name: 'User' # author_id
+  belongs_to :author, class_name: 'User'
+
   has_many :questions
   has_many :tests_users
   has_many :users, through: :tests_users
