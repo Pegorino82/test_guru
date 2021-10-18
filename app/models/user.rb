9d438fee42268d6279c744a5c6ea646b7029a2c2
Pegorino82 <eshkryabin@yandex.ru>
2021-10-18 18:20:27 +0300

    Update User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   has_many :tests, through: :results, dependent: :destroy
 
   def tests_by_level(level)
-    self.tests.where(level: level).order(title: :DESC).pluck(:title)
+    tests.where(level: level)
   end
 end
