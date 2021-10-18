77a25c12241f47c76c49c2c32fe664a49dd663da
Pegorino82 <eshkryabin@yandex.ru>
2021-10-18 18:23:22 +0300

    Code style

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,7 +6,7 @@ class Test < ApplicationRecord
 
   has_many :questions, dependent: :destroy
   has_many :results, dependent: :destroy
-  has_many :users, through: :results, dependent: :destroy
+  has_many :users, through: :results
 
   def self.by_category(title)
     joins(:category)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   has_many :authorship, class_name: "Test", foreign_key: "author_id", dependent: :nullify
   has_many :results, dependent: :destroy
-  has_many :tests, through: :results, dependent: :destroy
+  has_many :tests, through: :results
 
   def tests_by_level(level)
     tests.where(level: level)
