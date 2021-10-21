7dfdc6e3b5ef26d40d4941c083e34dd273db663d
Pegorino82 <eshkryabin@yandex.ru>
2021-10-21 22:59:41 +0300

    Validate answers amount for Question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
+  validate :validate_answers_amount
 
   scope :correct, -> { where(correct: true) }
+
+  private
+
+  def validate_answers_amount
+    errors.add(:answers_amount) unless (1..3).include?(question.answers.count)
+  end
 end
