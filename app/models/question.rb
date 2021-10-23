c0151e2e084fd50dba5b2d98a755b09c00f4a428
Pegorino82 <eshkryabin@yandex.ru>
2021-10-23 18:51:31 +0300

    Revert "Validate answer presence for question"
    This reverts commit 845fa560840f78b0691b8fc994c13383472e0ed6.

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,12 +6,4 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :body, presence: true
-
-  validate :validate_answers_amount, on: :create
-
-  private
-
-  def validate_answers_amount
-    errors.add(:base, 'Question must has at least one answer.') if answers.count.zero?
-  end
 end
