a83874d8c88444b26a5b77f6ecd991b42e4aeb29
Pegorino82 <eshkryabin@yandex.ru>
2021-10-29 20:02:12 +0300

    Add helpers

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module TestsHelper
+  LEVELS = { (0..2) => :easy, (2..4) => :middle, (5..) => :hard }.freeze
+
+  def test_level(test)
+    LEVELS.each do |level, text|
+      return text if level.include?(test.level)
+    end
+  end
 end
