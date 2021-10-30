4ad40cbb28a462d460a0ed7544202b4121f28b34
Pegorino82 <eshkryabin@yandex.ru>
2021-10-30 15:43:18 +0300

    Test level helper refactoring

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -4,8 +4,6 @@ module TestsHelper
   LEVELS = { (0..2) => :easy, (2..4) => :middle, (5..) => :hard }.freeze
 
   def test_level(test)
-    LEVELS.each do |level, text|
-      return text if level.include?(test.level)
-    end
+    LEVELS.detect { |level, _| level.include?(test.level) }.last
   end
 end
