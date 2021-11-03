00d2325c4471c1be5ea3d7c336467d105574a2b8
Pegorino82 <eshkryabin@yandex.ru>
2021-11-03 19:09:46 +0300

    Update result

--- app/models/result.rb
+++ app/models/result.rb
@@ -39,8 +39,7 @@ class Result < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
-    answer_ids = [] if answer_ids.nil?
-    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+    correct_answers.ids.sort == Array(answer_ids).map(&:to_i).sort
   end
 
   def correct_answers
