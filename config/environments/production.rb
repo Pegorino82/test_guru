bd31e1cc0101f83251eb770f66b5134f60b2e29a
Pegorino82 <eshkryabin@yandex.ru>
2021-11-29 15:35:50 +0300

    Rename app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test_guru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://fathomless-basin-02645.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://testguru33.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_ADDRESS'],
