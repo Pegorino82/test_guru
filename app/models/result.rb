c11ffa1e5a011db6403d0150305e3bf018c5d9df
Pegorino82 <eshkryabin@yandex.ru>
2021-12-04 12:30:22 +0300

    Add result column to results

--- app/models/result.rb
+++ app/models/result.rb
@@ -8,6 +8,9 @@ class Result < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_save :set_result
+
+  scope :passed, -> { where('result >= ?', SUCCESS_SCORE )}
 
   def completed?
     current_question.nil?
@@ -33,6 +36,10 @@ class Result < ApplicationRecord
   end
 
   private
+
+  def set_result
+    self.result = success_percentage
+  end
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_184658) do
+ActiveRecord::Schema.define(version: 2021_12_04_092128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2021_12_02_184658) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "current_question_id"
+    t.integer "result", default: 0
     t.index ["current_question_id"], name: "index_results_on_current_question_id"
     t.index ["test_id"], name: "index_results_on_test_id"
     t.index ["user_id"], name: "index_results_on_user_id"
